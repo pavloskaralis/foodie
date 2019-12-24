522f5dccd02d4dfce017920801fe7f6b76895d43
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2019-12-24 16:27:10 -0500

    corrected list get

--- backend/controllers/list.js
+++ backend/controllers/list.js
@@ -4,8 +4,13 @@ const router = express.Router();
 const List = require('../models/list.js');
 
 //Routes
+//returns all lists associated with client username
 router.get('/:username', (req, res) => {
-    res.send('test')
+    List.find({users: {$in: [req.params.username]}})
+    .then(lists => res.json(
+        {username: req.params.username, 
+            lists: lists
+        }));
 })
 
 module.exports = router; 
