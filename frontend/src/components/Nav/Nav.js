6d785e37bcd2a711e8f17abc825272adf17fcd26
Anna <anya@Annas-MBP.wavecable.com>
2020-01-01 20:31:19 -0800

    footer + nav complete

--- frontend/src/components/Nav/Nav.js
+++ frontend/src/components/Nav/Nav.js
@@ -9,12 +9,12 @@ class Nav extends Component {
                 
                     <div alt="avocado" ><img className='avocado' src='https://img.icons8.com/color/2x/avocado.png' alt="avocado" /><p>FOODIE</p></div>
                     <div></div>
-                    <div></div>
+                    
                 
-                    {<a href="/">Home</a>}
-                    {this.props.isLoggedIn ? <a href="/shopping-lists">Shopping Lists</a> : <a href="/login">Log In</a>}
-                    {this.props.isLoggedIn && <a href="/new-list">New List</a>}
-                    {this.props.isLoggedIn ? <a href="/" onClick={this.props.handleLogOut}>Log Out</a> : <a href="/#signup">Register</a> }
+                    {<a  className='links' href="/">Home</a>}
+                    {this.props.isLoggedIn ? <a className='links' href="/shopping-lists">Shopping Lists</a> : <a className='links' href="/login">Log In</a>}
+                    {this.props.isLoggedIn && <a className='links' href="/new-list">New List</a>}
+                    {this.props.isLoggedIn ? <a className='links' href="/" onClick={this.props.handleLogOut}>Log Out</a> : <a className='links' href="/#signup">Register</a> }
                 
                 </div>
             </nav>
