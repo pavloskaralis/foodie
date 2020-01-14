cf2baa8475c9e27c6cb526944d27ae3c9fcf084f
Pavlos Karalis <52301933+pavloskaralis@users.noreply.github.com>
2020-01-14 01:40:46 -0500

    Update App.js

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -13,6 +13,7 @@ import Update from '../Update/Update.js'
 import Share from '../Share/Share.js'
 import './App.css'
 
+//app will keep state and methods for login/signup/logout
 class App extends Component {
   state = {
     isLoggedIn: false,
@@ -21,7 +22,7 @@ class App extends Component {
 
   componentDidMount = () => {
     if(localStorage.token) {
-      axios.get('http://localhost:3001/user/verify/' + localStorage.token)
+      axios.get('https://foodie-list-app-backend.herokuapp.com/user/verify/' + localStorage.token)
       .then(response => this.setState({
         isLoggedIn: true, 
         username: response.data.username, 
@@ -46,11 +47,13 @@ class App extends Component {
       <React.Fragment>
         <Nav isLoggedIn={this.state.isLoggedIn} handleLogOut={this.handleLogOut}/>        
         <Switch>
-          {this.state.isLoggedIn && <Route path={'/shopping-lists/:id'} render={()=> <Show listID={this.state.listID}/>}/>}
-          {this.state.isLoggedIn && <Route path={'/shopping-lists'} render={()=> <Index selectList={this.selectList} username={this.state.username}/>}/>}
-          {this.state.isLoggedIn && <Route path={'/new-list'} render={()=> <Create username={this.state.username}/>}/>}
-          {this.state.isLoggedIn && <Route path={'/update-list/:id'} render={()=> <Update username={this.state.username}/>}/>}
-          {this.state.isLoggedIn && <Route path={'/share-list/:id'} render={()=> <Share username={this.state.username}/>}/>}
+          {this.state.isLoggedIn && <>
+            <Route path={'/shopping-lists/:id'} render={()=> <Show listID={this.state.listID}/>}/>
+            <Route path={'/shopping-lists'} render={()=> <Index selectList={this.selectList} username={this.state.username}/>}/>
+            <Route path={'/new-list'} render={()=> <Create username={this.state.username}/>}/>
+            <Route path={'/update-list/:id'} render={()=> <Update username={this.state.username}/>}/>
+            <Route path={'/share-list/:id'} render={()=> <Share username={this.state.username}/>}/>
+          </>}
           {!this.state.isLoggedIn && <Route path={'/login'} render={()=> <Login resetApp={this.componentDidMount}/>}/>}
           <Route path={'/'} render={()=> <Home resetApp={this.componentDidMount} handleSignUp={this.handleSignUp} isLoggedIn={this.state.isLoggedIn}/>}/>
         </Switch>
